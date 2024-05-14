d7bfe014ceecc2a3723b07e8ddca048b1a3ced82
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-05-14 15:19:45 +0530

    auto pushhh

--- main.tf
+++ main.tf
@@ -2,9 +2,11 @@ module "alb-public" {
   source                 = "./vendor/modules/alb"
   ENV                    = var.ENV
   ALB_NAME               = "roboshop-${var.ENV}-public"
+  INTERNAL               = false
 }
 module "alb-private" {
   source                 = "./vendor/modules/alb"
   ENV                    = var.ENV
   ALB_NAME               = "roboshop-${var.ENV}-private"
+  INTERNAL               = true
 }
