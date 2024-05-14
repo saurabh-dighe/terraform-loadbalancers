e36d11b478239f4db21f133f339c4a24baa17cd2
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-05-14 14:23:47 +0530

    auto pushhh

--- env-dev/dev.tfvars
+++ env-dev/dev.tfvars
@@ -2,8 +2,6 @@ ENV                 = "dev"
 VPC_CIDR            = "10.0.0.0/24"
 PUBLIC_SUBNET_CIDR  = ["10.0.0.0/26", "10.0.0.64/26"]
 PRIVATE_SUBNET_CIDR = ["10.0.0.128/26", "10.0.0.192/26"]
-ZONE                = ["us-east-1a", "us-east-1b"]
 DEFAULT_VPC_CIDR    = "172.31.0.0/16"
-DEFAULT_VPC_ID      = "vpc-016ae4359b34dde91"
-DEFAULT_VPC_RT      = "rtb-0f809576b0196d121"
+
 
--- vars.tf
+++ vars.tf
@@ -1,16 +1,7 @@
+variable "ENV" {}
 variable "VPC_CIDR"{}
 variable "PUBLIC_SUBNET_CIDR"{}
 variable "PRIVATE_SUBNET_CIDR"{}
-variable "ENV" {}
-variable "ZONE" {}
 variable "DEFAULT_VPC_CIDR" {}
-variable "DEFAULT_VPC_ID" {}
-variable "DEFAULT_VPC_RT" {}
 
-#R52 Variables
-# variable "PRIVATE_HOSTEDZONE_ID" {}
-# variable "PRIVATE_HOSTEDZONE_NAME" {}
-
-variable "PUBLIC_HOSTEDZONE_ID" {}
-variable "PUBLIC_HOSTEDZONE_NAME" {}
 
