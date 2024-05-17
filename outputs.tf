d49c464213d9e783eea94fd63630e45284ec847a
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-05-17 14:03:07 +0530

    Auto push

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,10 @@
+output "PUBLIC_ALB_ARN" {
+  value = module.alb-public.ALB_ARN
+}
+output "PRIVATE_ALB_ARN" {
+  value = module.alb-private.ALB_ARN
+}
+
 # output "VPC_ID" {
 #   value = module.alb.VPC_ID
 # }
