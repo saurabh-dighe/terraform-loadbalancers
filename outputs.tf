c8d4076dea4e4aaa65c23470c0346da8b268e55e
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-05-18 22:54:20 +0530

    Auto push

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,20 @@
+output "PRIVATE_ALB_ARN" {
+  value = module.alb-private.ALB_ARN
+}
 output "PUBLIC_ALB_ARN" {
   value = module.alb-public.ALB_ARN
 }
-output "PRIVATE_ALB_ARN" {
-  value = module.alb-private.ALB_ARN
+output "PRIVATE_ALB_ADDRESS" {
+  value = module.alb-private.ALB_ADDRESS
+}
+output "PUBLIC_ALB_ADDRESS" {
+  value = module.alb-public.ALB_ADDRESS
+}
+
+#Listener Outputs
+output "PRIVATE_LISTENER_ARN" {
+  value = aws_lb_listener.alb-private.PRIVATE_LISTENER_ARN
+}
+output "PUBLIC_LISTENER_ARN" {
+  value = aws_lb_listener.alb-public.PUBLIC_LISTENER_ARN
 }
